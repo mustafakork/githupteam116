8886f648483d7554875d4d960b69d14b74949ce8
mustafakork <mstfbsrysrykb60@gmail.com>
2023-04-26 23:23:33 +0300

    deneme3

--- src/deneme.java
+++ src/deneme.java
@@ -2,5 +2,7 @@ public class deneme {
     public static void main(String[] args) {
         System.out.println("deneme");
         System.out.println("deneme2");
+        System.out.println("deneme3");
+
     }
 }
